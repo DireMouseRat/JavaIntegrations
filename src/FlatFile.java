2fc45f76270590ec6b93818e7ea87a9b7a52ee07
Joshua Meis <joshuadmeis@gmail.com>
2021-12-23 14:53:38 -0600

    Flat File class actually works now.

--- src/FlatFile.java
+++ src/FlatFile.java
@@ -1,16 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class FlatFile {
     private List<String> header = new ArrayList<>();
     private final List<List<String>> records = new ArrayList<>();
 
-    public void read(String filePath, String delimiter, boolean hasHeader) throws FileNotFoundException {
+    public FlatFile(String filePath, String delimiter, boolean hasHeader) throws FileNotFoundException {
         Scanner sc = new Scanner(new File(filePath));
         if(hasHeader) {
             header = Arrays.asList(sc.nextLine().split(delimiter));
@@ -21,28 +17,84 @@ public class FlatFile {
         sc.close();
     }
 
-    public HashMap<String, String> getDictionary(String id, String value) {
+    // Using 2 fields as the key-value pair, return a dictionary (HashMap) from the FlatFile
+    // Duplicate keys are overwritten
+    public HashMap<String, String> getDictionary(String key, String value) throws IndexOutOfBoundsException{
+        int keyIndex = getFieldIndex(key);
+        int valueIndex = getFieldIndex(value);
         HashMap<String, String> dictionary = new HashMap<>();
-        if(header.isEmpty()) {
-            return null;
+        for(List<String> record : records) {
+            dictionary.put(record.get(keyIndex), record.get(valueIndex));
         }
-        else {
-            int keyIndex = header.indexOf(id);
-            int valueIndex = header.indexOf(value);
-            if(keyIndex == -1 || valueIndex == -1)
-            {
-                return null;
-            }
-            else {
-                for(List<String> record : records) {
-                    dictionary.put(record.get(keyIndex), record.get(valueIndex));
-                }
-                return dictionary;
-            }
-        }
+        return dictionary;
     }
 
-    public List<List<String>> Records(){
-        return new ArrayList<>(records);
+    public List<String> firstRecord() throws IndexOutOfBoundsException{
+        return records.get(0);
+    }
+
+    //Requires header
+    public String firstRecordValueAt(String fieldName) throws IndexOutOfBoundsException{
+        int fieldIndex = getFieldIndex(fieldName);
+        return firstRecord().get(fieldIndex);
+    }
+
+    public boolean hasNextRecord(){
+        return records.iterator().hasNext();
+    }
+
+    public List<String> nextRecord() throws NoSuchElementException {
+        return records.iterator().next();
+    }
+
+    public int recordCount(){
+        return records.size();
+    }
+
+    public int fieldCount(){
+        if(hasHeader()){
+            return header.size();
+        }
+        return firstRecord().size();
+    }
+
+    public boolean hasHeader(){return !header.isEmpty();}
+    public boolean noHeader(){return header.isEmpty();}
+
+    //Requires header
+    public boolean hasDuplicates(String fieldName) throws IndexOutOfBoundsException {
+        return hasDuplicates(getFieldIndex(fieldName));
+    }
+
+    public boolean hasDuplicates(int fieldIndex) throws IndexOutOfBoundsException {
+        HashSet<String> set = new HashSet<>();
+        for (List<String> record : records)
+            if (!set.add(record.get(fieldIndex)))
+                return true;
+        return false;
+    }
+
+    //Requires header
+    public List<String> getFieldNames() throws IndexOutOfBoundsException{
+        assertHeader();
+        return header;
+    }
+
+    private void assertHeader() throws IndexOutOfBoundsException{
+        if(noHeader()) throw new IndexOutOfBoundsException("Header not found");
+    }
+
+    //Requires header
+    public int getFieldIndex(String fieldName) throws IndexOutOfBoundsException {
+        assertHeader();
+        int fieldIndex = header.indexOf(fieldName);
+        if(fieldIndex == -1) throw new IndexOutOfBoundsException("Field not found");
+        return fieldIndex;
+    }
+
+    //Requires header
+    public String getFieldName(int fieldIndex) throws IndexOutOfBoundsException{
+        assertHeader();
+        return header.get(fieldIndex);
     }
 }
